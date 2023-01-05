5409e912a86275c3074ab3d2f0f14ca26c98549a
18kazee <18kazee2415@gmail.com>
2023-01-06 01:00:22 +0900

    Add: フラッシュメッセージの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to(login_path, notice: 'ユーザーの作成に成功しました')
+      redirect_to(login_path, success: t('.success'))
     else
-      flash.now[:alert] = 'ユーザーの作成に失敗しました'
+      flash.now[:danger] = t('.fail')
       render :new
     end
   end
