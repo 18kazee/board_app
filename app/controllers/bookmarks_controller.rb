4c2c11aab0709a7086c040d0ab91e1e95b20da62
18kazee <18kazee2415@gmail.com>
2023-01-26 00:49:29 +0900

    bookmark機能パフォーマンス修正

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,12 +3,12 @@ class BookmarksController < ApplicationController
   def create
     board = Board.find(params[:board_id])
     current_user.bookmark(board)
-    redirect_back fallback_location: :root, success: t('defaults.message.bookmark')
+    redirect_back fallback_location: :root, success: t('.success')
   end
 
   def destroy
     board = current_user.bookmarks.find(params[:id]).board
     current_user.unbookmark(board)
-    redirect_back fallback_location: :root, success: t('defaults.message.unbookmark')
+    redirect_back fallback_location: :root, success: t('.success')
   end
 end
